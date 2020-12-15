2ba997a4732e7a28a2360de62f5e84e58ab7405b
chetas11 <cbirmole@gmail.com>
2020-12-15 18:25:12 +0530

    complete

--- dist/scripts.13aae5c5.js
+++ dist/scripts.13aae5c5.js
@@ -29785,8 +29785,75 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 var App = function App() {
-  var tasks = ["Buy House", "Buy Car", "Go to katar"];
+  var _useState = (0, _react.useState)([{
+    name: "Buy House",
+    isComplete: false
+  }, {
+    name: "Buy Car",
+    isComplete: false
+  }]),
+      _useState2 = _slicedToArray(_useState, 2),
+      tasks = _useState2[0],
+      setTasks = _useState2[1];
+
+  var _useState3 = (0, _react.useState)("New Task"),
+      _useState4 = _slicedToArray(_useState3, 2),
+      newTaskText = _useState4[0],
+      setnewTaskText = _useState4[1];
+
+  var onInputChnage = function onInputChnage(e) {
+    setnewTaskText(e.target.value);
+  };
+
+  var AddnewTask = function AddnewTask(e) {
+    if (newTaskText) {
+      setTasks([].concat(_toConsumableArray(tasks), [{
+        name: newTaskText,
+        isComplete: false
+      }]));
+      setnewTaskText("");
+    }
+  };
+
+  var toggleTask = function toggleTask(ClickedTasks) {
+    setTasks(tasks.map(function (task, tabIndex) {
+      if (tabIndex === ClickedTasks) {
+        return _objectSpread(_objectSpread({}, task), {}, {
+          isComplete: !task.isComplete
+        });
+      }
+
+      return task;
+    }));
+  };
+
   return /*#__PURE__*/_react.default.createElement("div", {
     className: "container mt-5"
   }, /*#__PURE__*/_react.default.createElement("div", {
@@ -29798,14 +29865,25 @@ var App = function App() {
   }, /*#__PURE__*/_react.default.createElement("h1", {
     className: "text-light text-center bg-dark"
   }, "ToDoList"), /*#__PURE__*/_react.default.createElement("input", {
+    type: "text",
+    value: newTaskText,
+    onChange: onInputChnage,
     className: "form-control my-4",
     placeholder: "Enter new task"
   }), /*#__PURE__*/_react.default.createElement("button", {
+    onClick: AddnewTask,
     className: "btn btn-md btn-primary"
   }, "+Add"), /*#__PURE__*/_react.default.createElement("ul", {
     className: "mt-3"
-  }, tasks.map(function (each) {
-    return /*#__PURE__*/_react.default.createElement("li", null, each);
+  }, tasks.map(function (task, taskindex) {
+    var ontaskClicked = function ontaskClicked() {
+      toggleTask(taskindex);
+    };
+
+    return /*#__PURE__*/_react.default.createElement("li", {
+      onClick: ontaskClicked,
+      key: taskindex
+    }, task.name, " ", task.isComplete ? "✔️" : "❌");
   }))), /*#__PURE__*/_react.default.createElement("div", {
     className: "col-lg-4 col-md-4 col-sm-4"
   })));
@@ -29840,7 +29918,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49568" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50514" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/scripts.js
+++ src/scripts.js
@@ -3,7 +3,33 @@ import ReactDOM from "react-dom";
 
 const App = () =>{
 
-    const tasks = ["Buy House", "Buy Car", "Go to katar"]
+    const [tasks, setTasks] = useState([{name:"Buy House",isComplete: false}, {name:"Buy Car",isComplete:false}])
+    const [newTaskText, setnewTaskText] = useState("New Task");
+    const onInputChnage = (e) =>{
+        setnewTaskText(e.target.value)
+    }
+    const AddnewTask = (e) =>{
+        if(newTaskText){
+        setTasks([...tasks, {
+            name: newTaskText,
+            isComplete:false
+        }]);
+        setnewTaskText("")
+        }
+    }
+    const toggleTask = ((ClickedTasks) =>{
+        setTasks(
+            tasks.map((task, tabIndex)=>{
+                if(tabIndex===ClickedTasks){
+                    return{
+                        ...task,
+                        isComplete:!task.isComplete
+                    }
+                }
+                return task
+            })
+        )
+    })
 
     return(
         <div className="container mt-5">
@@ -11,11 +37,14 @@ const App = () =>{
                 <div className="col-lg-4 col-md-4 col-sm-4"></div>
                 <div className="col-lg-4 col-md-4 col-sm-4">
                     <h1 className="text-light text-center bg-dark">ToDoList</h1>
-                    <input className="form-control my-4" placeholder="Enter new task"></input>
-                    <button className="btn btn-md btn-primary">+Add</button>
+                    <input type="text" value={newTaskText} onChange={onInputChnage} className="form-control my-4" placeholder="Enter new task"></input>
+                    <button onClick = {AddnewTask} className="btn btn-md btn-primary">+Add</button>
                     <ul className="mt-3">
-                        {tasks.map((each)=>{
-                            return <li>{each}</li>
+                        {tasks.map((task, taskindex)=>{
+                            const ontaskClicked = () =>{
+                                toggleTask(taskindex)
+                            };
+                            return <li onClick={ontaskClicked} key={taskindex}>{task.name} {task.isComplete ? "✔️" : "❌"}</li>
                         })}
                     </ul>
                 </div>
