7d3d56856ccad86e605f747335808712398d4ab1
chetas11 <cbirmole@gmail.com>
2020-12-15 15:32:21 +0530

    Started

---
+++ src/scripts.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+import ReactDOM from "react-dom";
+
+const App = () =>{
+    return(
+        <h1>Hello</h1>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector('#react-root'))
